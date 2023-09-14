d85f5a05ef12318f38732e4adb652f55bb90b53e
Кирилл Латышов <kirilllatyshov99@gmail.com>
2023-09-14 14:56:21 +0300

    Check without cmake

--- script.sh
+++ script.sh
@@ -2,21 +2,21 @@
 
 echo  "Start CMake installation"
 
-apt update
-#Cmake install
-apt install wget make gcc g++ openssl -y
-#cd /root/
-wget https://github.com/Kitware/CMake/releases/download/v3.27.4/cmake-3.27.4.tar.gz
-tar -zxvf cmake-3.27.4.tar.gz
-cd cmake-3.27.4/
-./bootstrap
-make
-make install
-cd ..
-export PATH="`pwd`/cmake-3.27.4/bin:$PATH"
-cmake --version
-
-echo "Cmake install!"
+#apt update
+##Cmake install
+apt install wget make gcc g++ openssl tcpdump -y
+##cd /root/
+#wget https://github.com/Kitware/CMake/releases/download/v3.27.4/cmake-3.27.4.tar.gz
+#tar -zxvf cmake-3.27.4.tar.gz
+#cd cmake-3.27.4/
+#./bootstrap
+#make
+#make install
+#cd ..
+#export PATH="`pwd`/cmake-3.27.4/bin:$PATH"
+#cmake --version
+#
+#echo "Cmake install!"
 
 echo "Start SV sending!"
 chmod +x ./ISNetworkBench
